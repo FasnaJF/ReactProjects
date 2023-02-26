8ab539394e18c091f1461a60650287a890746e18
jffasna <fasna@rhino-partners.com>
2023-02-26 22:06:29 +0530

    01-signature-app

---
+++ src/projects/01-e-signature-app/ESignatureApp.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import Title from "../components/Title";
+
+export default function ESignatureApp() {
+  const [name, setName] = useState("Your Signature");
+  const [date, setDate] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  }
+
+  const inputStyle = {
+    border: "none",
+    borderBottom: "2px dotted",
+    outline: "none",
+    padding: "0.35rem 0",
+  };
+  document.body.style.background = "#eee";
+  return (
+    <div className="container text-center">
+      <Title classes={"title "} text={name} />
+      <Title classes={"sub-title mb-4"} text={!date? "DoB": date} />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nulla
+        autem optio perferendis ullam error ducimus itaque dolorum, neque hic
+        placeat, repellendus veniam tempora, quos libero cumque impedit
+        consectetur at?
+      </p>
+      <footer
+        className="d-flex"
+        style={{
+          justifyContent: "space-around",
+          position: " relative",
+          top: "40vh",
+        }}
+      >
+        <input type="date" value={date} style={inputStyle} onChange={handleDateChange}/>
+        <input type="text" value={name} style={inputStyle} onChange={handleNameChange}/>
+      </footer>
+    </div>
+  );
+}
